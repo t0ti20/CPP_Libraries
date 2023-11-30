a774aeff0db929fa75b471728d80999d47efb923
Khaled El-Sayed <t0ti20@icloud.com>
2023-11-30 09:35:44 +0200

    Update Print Lib

---
+++ Print.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************
+ *  FILE DESCRIPTION
+-----------------------
+ *  Author: Khaled El-Sayed @t0ti20
+ *  File: ./Print.cpp
+ *  Date: November 30, 2023
+ *  Description: This Is Default Test File For CPP Generator
+ *  Class Name: Print
+ *  (C) 2023 "@t0ti20". All rights reserved.
+*******************************************************************/
+/*****************************************
+-----------     INCLUDES     -------------
+*****************************************/
+#include "Print.hpp"
+/*****************************************
+----------    GLOBAL DATA     ------------
+*****************************************/
+namespace print
+{
+     using namespace std;
+     Print::Print()
+     {    
+          for(size_t Counter=0;Counter<10;Counter++)
+          {
+               cout << Welcome_Message << endl;
+          }
+     }
+     Print::~Print()
+     {
+          cout << "Goodbye" << endl;
+     }
+}
+/********************************************************************
+ *  END OF FILE:  ./Print.cpp
+********************************************************************/
---
+++ Print.hpp
@@ -0,0 +1,33 @@
+/*******************************************************************
+ *  FILE DESCRIPTION
+-----------------------
+ *  Author: Khaled El-Sayed @t0ti20
+ *  File: ./Print.hpp
+ *  Date: November 30, 2023
+ *  Description: This Is Default Test File For CPP Generator
+ *  Class Name: Print
+ *  (C) 2023 "@t0ti20". All rights reserved.
+*******************************************************************/
+/*****************************************
+------------    Include     --------------
+*****************************************/
+#include <iostream>
+#include <string>
+/*****************************************
+----------    GLOBAL DATA     ------------
+*****************************************/
+namespace print
+{
+class Print
+{
+public:
+     Print();
+     ~Print();
+private:
+     std::string Welcome_Message="Hello World !";
+protected:
+};
+}
+/********************************************************************
+ *  END OF FILE:  ./Print.hpp
+********************************************************************/
